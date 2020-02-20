61f9ac640bd96e58b48df07281b29a966279d632
min-jyo <minjyo95@gmail.com>
2020-02-20 22:39:25 +0900

    first commit

---
+++ app/snippets/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = (
+            'pk',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+            'created',
+        )
